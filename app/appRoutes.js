18e45bfab08a07d0bf1fa5c6c75fb2b26b42177f
GarethFentimen <garethfentimen@gmail.com>
2015-04-16 23:30:09 +0100

    Move out app routes

---
+++ app/appRoutes.js
@@ -0,0 +1,37 @@
+module.exports = function(router) {
+
+	router.get('/', function(req, res) {
+		var data = { title: 'Gareth Fentimen' };
+		res.render('home', data);
+	});
+
+	router.get('/home', function(req, res) {
+		//res.send('im the home page!'); 
+		var data = { title: 'Gareth Fentimen' };
+		res.render('home', data);
+	});
+
+	router.get('/blog', function(req, res) {
+		res.render('blog', { scripts: ['libs/lodash/dist/lodash.min.js',
+								'libs/angular/angular.min.js',
+								'libs/angular-animate/angular-animate.min.js',
+								'libs/angular-route/angular-route.min.js',
+								'libs/angular-sanitize/angular-sanitize.min.js',
+								'libs/restangular/dist/restangular.min.js',
+								'scripts/blog.js'] 
+					});
+	});
+
+	router.get('/about', function(req, res) {
+		var data = { title: "About me" };
+		res.render('about', data);	
+	});
+
+	router.get('/contact', function(req, res) {
+		res.render('contact', { 
+				title: "Contact me"			
+			});
+	});
+
+	return router;
+};
--- server.js
+++ server.js
@@ -26,42 +26,7 @@ var port = process.env.PORT || 3000; 		// set our port
 
 // API routes
 var router = require('./app/APIRoutes');
-
-// home page route (http://localhost:3000)
-router.get('/', function(req, res) {
-	//res.send('im the home page!');
-	var data = { title: 'Gareth Fentimen' };
-	res.render('home', data);
-});
-
-router.get('/home', function(req, res) {
-	//res.send('im the home page!');
-	var data = { title: 'Gareth Fentimen' };
-	res.render('home', data);
-});
-
-router.get('/blog', function(req, res) {
-	res.render('blog', { scripts: ['libs/lodash/dist/lodash.min.js',
-							'libs/angular/angular.min.js',
-							'libs/angular-animate/angular-animate.min.js',
-							'libs/angular-route/angular-route.min.js',
-							'libs/angular-sanitize/angular-sanitize.min.js',
-							'libs/restangular/dist/restangular.min.js',
-							'scripts/blog.js'] 
-				});
-});
-
-// about page route (http://localhost:8080/about)
-router.get('/about', function(req, res) {
-	var data = { title: "About me" };
-	res.render('about', data);	
-});
-
-router.get('/contact', function(req, res) {
-	res.render('contact', { 
-			title: "Contact me"			
-		});
-});
+router = require("./app/appRoutes.js")(router);
 
 // redirect for www to non-www
 app.all('/*', function(req, res, next) {
