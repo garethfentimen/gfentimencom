dbc0d3500c22f9d2064501ced75c195681f9d3d4
GarethFentimen <garethfentimen@gmail.com>
2015-05-11 22:20:40 +0100

    Everything broken writing unit tests!

--- app/appRoutes.js
+++ app/appRoutes.js
@@ -1,5 +1,30 @@
 module.exports = function(router) {
 
+	var basicDependencies = ['libs/angular/angular.min.js',
+								'libs/angular-ui-bootstrap-bower/ui-bootstrap.min.js',
+								'libs/angular-route/angular-route.min.js',
+								'libs/angular-sanitize/angular-sanitize.min.js'];
+
+	extraSpaDependencies = ['libs/angular-animate/angular-animate.min.js',
+							'libs/restangular/dist/restangular.min.js']
+
+	appScripts = ['scripts/blog.js'];
+
+	function unionArrays(arrayToBeAddedTo, arrayToAppend, arrayToAppend2) {
+		for (var value in arrayToAppend)
+		{
+			arrayToBeAddedTo.push(value);
+		}
+
+		for (var value in arrayToAppend)
+		{
+			arrayToBeAddedTo.push(value);
+		}
+		return arrayToBeAddedTo;
+	}
+
+	var completeSet = unionArrays(extraSpaDependencies, basicDependencies, appScripts);
+
 	router.get('/', function(req, res) {
 		var data = { title: 'Gareth Fentimen' };
 		res.render('home', data);
@@ -12,14 +37,7 @@ module.exports = function(router) {
 	});
 
 	router.get('/blog', function(req, res) {
-		res.render('blog', { scripts: ['libs/lodash/lodash.min.js',
-								'libs/angular/angular.min.js',
-								'libs/angular-ui-bootstrap-bower/ui-bootstrap.min.js',
-								'libs/angular-animate/angular-animate.min.js',
-								'libs/angular-route/angular-route.min.js',
-								'libs/angular-sanitize/angular-sanitize.min.js',
-								'libs/restangular/dist/restangular.min.js',
-								'scripts/blog.js'] 
+		res.render('blog', { scripts: completeSet
 					});
 	});
 
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,23 +1,2 @@
-"use strict";
 
-$(document).ready(function() {
 
-	var url = window.location.href;
-	var splitUrl = url.split("/");
-	var location = splitUrl[splitUrl.length-1];
-	
-	//console.log(location);
-
-	$(".nav li").each(function(index)
-	{
-		
-		var menuText = $(this).text().toLowerCase();
-		if (menuText === location || (location === "" && menuText === "home"))
-		{
-			$(this).addClass("active");
-		} else {
-			$(this).removeClass("active");
-		}
-	});
-
-});
