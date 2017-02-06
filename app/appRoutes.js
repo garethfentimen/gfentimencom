815486ce74e7f488a0878e5b525fb8f41c127abc
garethfentimen <garethfentimen@gmail.com>
2017-02-06 16:13:17 -0500

    Quick wins: fix year, update summary info, fix serverside data

--- app/appRoutes.js
+++ app/appRoutes.js
@@ -1,31 +1,37 @@
 
 module.exports = function(router) {
+	var standardServerData = function (aTitle) {
+		if (!aTitle)
+		{
+			aTitle = 'Gareth Fentimen - Software Developer';
+		}
+
+		return {
+			title: aTitle, 
+			year: new Date().getUTCFullYear()
+		}
+	}
 
 	router.get('/', function(req, res) {
-		var data = { title: 'Gareth Fentimen - Software Developer' };
-		res.render('home', data);
+		res.render('home', standardServerData());
 	});
 
 	router.get('/home', function(req, res) {
-		var data = { title: 'Gareth Fentimen - Software Developer' };
-		res.render('home', data);
+		res.render('home', standardServerData());
 	});
 
 	router.get('/blog', function(req, res) {
-		res.render('blog', {
-        	scripts:  ['public/build/scripts/dep.min.js', 'public/build/scripts/app.min.js']
-		});
+		var data = standardServerData();
+		data.scripts = ['public/build/scripts/dep.min.js', 'public/build/scripts/app.min.js'];
+		res.render('blog', data);
 	});
 
 	router.get('/about', function(req, res) {
-		var data = { title: 'Gareth Fentimen - Software Developer' };
-		res.render('about', data);	
+		res.render('about', standardServerData());	
 	});
 
 	router.get('/contact', function(req, res) {
-		res.render('contact', { 
-				title: "Contact me"			
-			});
+		res.render('contact', standardServerData("Contact me"));
 	});
 
 	return router;
