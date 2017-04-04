e19ec144064781eddb4a9680d79366357fa6afcd
garethfentimen <garethfentimen@gmail.com>
2017-04-04 13:24:15 -0400

    Finally fixed the menu for mobile

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,18 +1,35 @@
 (function() {
 	"use strict";
 
-	// don't use jquery - fine for ie9+
+	// For ie9+
 	document.addEventListener('DOMContentLoaded', function(){
 
-		var url = window.location.href;
-		var splitUrl = url.split("/");
-		var location = splitUrl[splitUrl.length-1];
+		var url = window.location.href,
+			splitUrl = url.split("/"),
+			location = splitUrl[splitUrl.length-1],
+			subLocation = splitUrl[splitUrl.length-2].replace("#", "");
+
+		var collapsedNavButton = document.querySelectorAll(".navbar-toggle");
+		
+		collapsedNavButton[0].addEventListener("click", function() {
+			var navbar = document.querySelectorAll(".navbar-collapse")[0];
+			if (!navbar.classList) {
+				return;
+			}
+
+			if (navbar.classList.length > 1)
+			{
+				removeClass(navbar, "collapse");
+			} else {
+				addClass(navbar, "collapse");
+			}
+		});
 
 		var elements = document.querySelectorAll(".nav li");
 		Array.prototype.forEach.call(elements, function(el){
 			
 			var menuText = el.textContent.toLowerCase();
-			if (menuText === location || (location === "" && menuText === "home")) {
+			if (menuText === location || subLocation === menuText || (location === "" && menuText === "home")) {
 				addClass(el, "active");
 			} else {
 				removeClass(el, "active");
